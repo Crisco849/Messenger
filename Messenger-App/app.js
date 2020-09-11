bfeb2c8f867831dfd2ff9598bbfb4731e1300b82
Crisco849 <SecondSlasher123@Gmail.com>
2020-09-10 19:32:41 -0500

    new online working build

---
+++ Messenger-App/app.js
@@ -0,0 +1,109 @@
+// Copyright 2017 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+// [START gae_flex_quickstart]
+//const express = require('express');
+//const app = express();
+
+
+// new
+//const app = require('express')();
+// trying out ejs engine---- app.set('view engine', 'pug');
+const express = require('express')
+const app = express()
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+///// new
+
+// newest build==========
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+const rooms = {}
+// ======================
+
+app.get('/', (req, res) => {
+  // res.render('index.pug');
+  res.render('index', { rooms: rooms});
+  //res.status(200).send('Hello, world!').end();
+});
+
+// new-==================
+app.post('/room', (req, res) => {
+  if (rooms[req.body.room] != null) {
+    return res.redirect('/')
+  }
+  rooms[req.body.room] = { users: {} }
+  res.redirect(req.body.room)
+  // Send message that new room was created
+  io.emit('room-created', req.body.room)
+})
+
+app.get('/:room', (req, res) => {
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/')
+  }
+  res.render('room', { roomName: req.params.room })
+})
+
+// in original server.js: server.listen(8080) next if statement should do the same thing.
+if (module === require.main) {
+  const PORT = process.env.PORT || 65080;
+  server.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+  });
+}
+
+io.on('connection', socket => {
+  socket.on('new-user', (room, name) => {
+    socket.join(room)
+    rooms[room].users[socket.id] = name
+    socket.to(room).broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', (room, message) => {
+    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    getUserRooms(socket).forEach(room => {
+      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+      delete rooms[room].users[socket.id]
+    })
+  })
+})
+
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name)
+    return names
+  }, [])
+}
+//=======================
+
+
+
+
+// Start the server
+/*const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+}); */
+// [END gae_flex_quickstart]
+
+module.exports = server;
