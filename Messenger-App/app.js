ea307b5ed20f3cbbdd66cdfcf7d0844405b8c123
Collin Varisco <collinvarisco@Gmail.com>
2020-11-11 09:55:49 -0600

    updated comments

--- Messenger-App/app.js
+++ Messenger-App/app.js
@@ -1,28 +1,38 @@
 "use strict";
-
+// Express is used as a dependency for NodeJS HTML/Javascript pages.
+// NodeJS and Express dependency files are stored on virtual machine.
+// Express is initialized here.
 const express = require("express");
 const app = express();
+// Initializing http server
 const server = require("http").Server(app);
+// Initializing server socket functionality with socket.io
 const io = require("socket.io")(server);
 
+// Express view engine initialization.
 app.set("views", "./views");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+// Expanding array that stores created rooms.
 const rooms = {};
 var count = 0;
+
+// Uses express engine to render the home page 'index.ejs'
 app.get("/", (req, res) => {
   res.render("index", { rooms: rooms });
 });
 
+// Receives room creation event from client side.
 app.post("/room", (req, res) => {
   if (rooms[req.body.room] != null) {
     return res.redirect("/");
   }
+  // store newly created room and initialize an expanding array of users.
   rooms[req.body.room] = { users: {} };
   res.redirect(req.body.room);
-  // Send message that new room was created
+  // Send message to the client that the new room was successfully created
   io.emit("room-created", req.body.room);
 });
 
@@ -33,14 +43,9 @@ app.get("/:room", (req, res) => {
   res.render("room", { roomName: req.params.room });
 });
 
-if (module === require.main) {
-  const PORT = process.env.PORT || 65080;
-  server.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-    console.log("Press Ctrl+C to quit.");
-  });
-}
+//PORT
 
+// Processes data from individual clients and delivers that data to the rest of the users in the room.
 io.on("connection", (socket) => {
   socket.on("new-user", (room, name) => {
     socket.join(room);
